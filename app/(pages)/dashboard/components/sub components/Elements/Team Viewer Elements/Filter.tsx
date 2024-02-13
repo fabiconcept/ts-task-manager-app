ec7eb60683251862e564015d2ebb377414da3751
Fabian Ajokubi <favourajokubi@gmail.com>
2024-02-13 14:35:48 +0100

    Implement ViewType Change

--- app/(pages)/dashboard/components/sub components/Elements/Team Viewer Elements/Filter.tsx
+++ app/(pages)/dashboard/components/sub components/Elements/Team Viewer Elements/Filter.tsx
@@ -1,8 +1,22 @@
+"use client"
 import { CiGrid2H, CiGrid41 } from "react-icons/ci";
 import SearchFeature from "../../side components/SearchFeature";
 import { FaArrowUpAZ } from "react-icons/fa6";
+import { useDispatch } from "react-redux";
+import { updateViewType } from "@/Redux Store/Slices/profiles/team/displayState";
+import { ViewType } from "@/lib/Enums";
 
 export default function Filter() {
+    const dispatch = useDispatch();
+
+    const handleSwitchToBoxView = () => {
+        dispatch(updateViewType(ViewType.BOX));
+    }
+    
+    const handleSwitchToListView = () => { 
+        dispatch(updateViewType(ViewType.LIST));
+     }
+
     return (
         <div className="p-4 sticky top-0 backdrop-blur-md">
             <p className="py-3 opacity-60">Team members</p>
@@ -14,8 +28,8 @@ export default function Filter() {
                     />
                 </div>
                 <div className="flex items-center gap-2">
-                    <span className="cursor-pointer hover:scale-110 active:scale-90 active:opacity-50" title="Box view"><CiGrid41 /></span>
-                    <span className="cursor-pointer hover:scale-110 active:scale-90 active:opacity-50" title="List view"><CiGrid2H /></span>
+                    <span onClick={handleSwitchToBoxView} className="cursor-pointer hover:scale-110 active:scale-90 active:opacity-50" title="Box view"><CiGrid41 /></span>
+                    <span onClick={handleSwitchToListView} className="cursor-pointer hover:scale-110 active:scale-90 active:opacity-50" title="List view"><CiGrid2H /></span>
                     <span className="cursor-pointer hover:scale-110 active:scale-90 active:opacity-50" title="Sort by"><FaArrowUpAZ /></span>
                 </div>
             </div>
--- app/(pages)/dashboard/components/sub components/Elements/Team Viewer Elements/TeamList.tsx
+++ app/(pages)/dashboard/components/sub components/Elements/Team Viewer Elements/TeamList.tsx
@@ -5,15 +5,10 @@ import TeamMember from "./TeamMember";
 import { useSelector } from "react-redux";
 import { echoDisplayList, echoViewType } from "@/Redux Store/Slices/profiles/team/displayState";
 import { ViewType } from "@/lib/Enums";
-import { echoTaskerProfilesActiveId, echoTaskerProfilesResponse } from "@/Redux Store/Slices/profiles";
-import { TaskerProfile } from "@/lib/Interfaces";
 
 export default function TeamList() {
     const teamList = useSelector(echoDisplayList);
     const viewType = useSelector(echoViewType);
-    const activeId = useSelector(echoTaskerProfilesActiveId);
-
-    const { tags, profiles } = useSelector(echoTaskerProfilesResponse);
 
     const gridViewMode = useMemo(() => {
         const boxView= "grid-cols-[repeat(auto-fill,minmax(10rem,1fr))]";
@@ -30,7 +25,7 @@ export default function TeamList() {
 
     return (
         <section className={clsx(`grid ${gridViewMode} gap-4 p-4`)}>
-            {teamList.map((member, index)=>(
+            {teamList.map((member)=>(
                 <TeamMember key={member.userId} data={member} />
             ))}
         </section>
